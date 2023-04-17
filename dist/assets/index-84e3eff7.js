1bc9ae57dfeccf78a95b51560a4138a32b74b444
zhfy <zhfyjeremy@163.com>
2023-04-17 15:08:35 +0800

    feat:modify

---
+++ dist/assets/index-84e3eff7.js
@@ -0,0 +1,1 @@
+import{ag as e,p as t,q as c}from"./index-a8835d9d.js";const s={},r={class:"flex-center text-white text-5xl bg-primary-default py-80"};function n(o,a){return t(),c("div",r,"This is About view")}const _=e(s,[["render",n]]);export{_ as default};
