4c3460178ed0df7644077671eea60d2595b1d316
zhfy <zhfyjeremy@163.com>
2023-05-29 11:09:32 +0800

    modify

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
-    <router-view />
+    <RouterView v-slot="{ Component }">
+        <template v-if="Component">
+            <Suspense>
+                <component :is="Component"></component>
+            </Suspense>
+        </template>
+    </RouterView>
 </template>
 <script lang="ts" setup></script>
 <style lang="scss" scoped></style>
--- src/layout/components/AppMain/index.vue
+++ src/layout/components/AppMain/index.vue
@@ -1,6 +1,8 @@
 <template>
-    <suspense>
-        <router-view />
-    </suspense>
+    <RouterView v-slot="{ Component, route }">
+        <template v-if="Component">
+            <component :is="Component" :key="route.fullPath" />
+        </template>
+    </RouterView>
 </template>
 <script lang="ts" setup></script>
