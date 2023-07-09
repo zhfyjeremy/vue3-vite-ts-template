9789e62743085b3d2c4173c6abbbd212b411f2dc
zhfy <zhfyjeremy@163.com>
2023-07-09 18:54:07 +0800

    feat: 表格使用示例分支描述

--- src/views/table/index.vue
+++ src/views/table/index.vue
@@ -21,10 +21,22 @@ const handleClick = (tab: TabsPaneContext) => {
                     type="primary"
                     href="https://blog.csdn.net/weixin_45291937/article/details/125523244"
                     target="_blank">
-                    可参考CSDN中的参数介绍
+                    Table表格基本封装 CSDN
                 </el-link>
             </div>
             <p class="text-sm text-gray-500">也可以参考 @/components/table/doc.md 中的参数介绍</p>
+            <div class="bg-gray-100 py-3 px-5 mt-4 rounded-md text-xs text-gray-500">
+                <p class="flex items-center mb-1.5 text-sm text-primary-default">
+                    <easy-icon icon="svg:branch" size="20" />
+                    <span>template_phase_2.0</span>
+                </p>
+                <p class="mb-1.5">template_phase_1.0分支： 使用 tempalte 封装, 最基本的功能，代码简单，方便自己扩展</p>
+                <p class="mb-1.5">
+                    template_phase_2.0分支： 使用 tempalte 封装, 在1.0 基础上做了优化。在编码时更好的类型支持，支持原
+                    element table 的属性和方法，并新增部分参数和事件。具体可查看文档 @/components/table/doc.md
+                </p>
+                <p>master分支： 使用 h 函数封装，功能有搜索， 编辑单元格等</p>
+            </div>
         </el-card>
         <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
             <el-tab-pane label="基本表格" name="table.base" />
