3ed562297f331a1c17067a31f4d2a6563a6d7d5a
zhfy <zhfyjeremy@163.com>
2023-07-09 18:03:05 +0800

    feat: 添加组件

---
+++ src/components/ClickOutside/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div ref="target" class="w-full">
+        <slot></slot>
+    </div>
+</template>
+<script lang="ts" setup>
+import { onClickOutside } from '@vueuse/core'
+const props = defineProps<{ disabled?: boolean }>()
+const emit = defineEmits(['mounted', 'clickOutside'])
+const target = ref<HTMLElement>()
+onClickOutside(target, (event: MouseEvent) => {
+    if (props.disabled) return
+    emit('clickOutside', event)
+})
+
+onMounted(() => {
+    emit('mounted')
+})
+</script>
+<style lang="scss" scoped></style>
